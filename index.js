1ad84bc23ddd27577ba91e92f11b0d056827dc1b
NMoore42 <nicholas.moore5@icloud.com>
2019-11-27 10:01:42 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(winArr){
+  const foundWin = winArr.find(winObj => winObj.result === "W" )
+  return foundWin ? foundWin.year : undefined
+}
